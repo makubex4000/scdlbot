21ee6bc674bf3b180793bb41b673a6b23fbdb013
makubex4000 <98877740+makubex4000@users.noreply.github.com>
2024-03-03 21:10:01 +0700

    Update __main__.py

--- scdlbot/__main__.py
+++ scdlbot/__main__.py
@@ -1213,7 +1213,7 @@ def download_url_and_send(
                     else:
                         source = url_obj.host.replace(".com", "").replace(".ru", "").replace("www.", "").replace("m.", "")
                     # TODO fix youtube id in []
-                    caption = "[{}]({}){}".format(bot.username.replace("_", r"\_"), source, url, addition.replace("_", r"\_"))
+                    caption = "{}[{}]({}){}", source, url, addition.replace("_", r"\_"))
                     if add_description:
                         caption += "\n\n" + add_description
                     # logger.debug(caption)
